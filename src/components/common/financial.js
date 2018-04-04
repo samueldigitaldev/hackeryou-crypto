db9dcc5308dd7a95e5a0f5140b35371a5512bb4f
Samuel <samuelwlai@gmail.com>
2018-04-04 18:46:38 -0400

    test

--- src/components/common/financial.js
+++ src/components/common/financial.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const Financial = (props) => {
+  console.log(props)
   return (
     <div>
       <h2>Restricted Dashboard</h2>
       <div>Net Amount Spent: {this.props.financialNet()}</div>
       <div>Net Value of Assets: {this.props.currentValue()}</div>
       <div>Return on Investment:
-        {this.props.financialNet() == 0.00 ? " 0.00" : (((this.props.currentValue() / this.props.financialNet()) -1)*100).toFixed(2)}%
+        {this.props.financialNet() === 0.00 ? " 0.00" : (((this.props.currentValue() / this.props.financialNet()) -1)*100).toFixed(2)}%
       </div>
     </div>
   )
